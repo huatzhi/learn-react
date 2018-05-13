b1ec5660923a6e694ffcb4fa031b858bb5e00244
Huat Zhi <huatzhi@gmail.com>
2018-05-13 13:19:35 +0800

    lect 31 back to JSX

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    let h1Tag = React.createElement('h1', null, 'Does this work now?');
-    return React.createElement('div', {className: 'App'}, h1Tag);
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+      </div>
+    );
   }
 }
 
