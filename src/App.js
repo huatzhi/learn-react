2e199bd97bce4b8c981766a2215f8541f09201bf
huat <huat@snsoft.my>
2018-05-22 14:08:40 +0800

    lect 46 - adding css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'; // webpack will handle the css import
 import Person from "./Person/Person";
 
 class App extends Component {
