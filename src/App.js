f08973370066790a96fb08cc125078f70f969713
huat <huat@snsoft.my>
2018-05-22 11:50:58 +0800

    lect 39 - state introduction

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = { // state is a reserved word for Component
+    persons: [
+      { name: 'Francium', age: 99 },
+      { name: 'Yui', age: 18 },
+      { name: 'Azure', age: 26 },
+    ]
+  }
+
+  // NOTE :: state can change, if it change it will update the DOM
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Francium" age="99">Ex evil lord but still evil</Person>
-        <Person name="Yui" age="18">I am an Angel</Person>
-        <Person name="AzureRedFrame" age="0.1"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
